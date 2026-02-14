82ab390a2e67832c2821363df8102b49df40b42a
Achanta Manoj Kumar <manojachanta7172@gmail.com>
2026-02-14 12:18:25 +0000

    added all files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header({ theme, setTheme }) {
       </div>
       <div className="ide__status">
         <div className="shoutout">
-          <strong className="personal-brand">Made with <span style={{color: "#e25555", fontSize: "14px"}}>❤</span> by {"Manoj Kumar Achanta"}</strong>
+          <strong className="personal-brand">Made with <span style={{color: "#e25555", fontSize: "14px"}}>❤</span> by <span className="name">{"Manoj Kumar Achanta"}</span></strong>
         </div>
         <div className="live-container">
           <span className="dot" /> Live
